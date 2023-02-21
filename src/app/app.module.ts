4ed2c74693a7c4a5cd401904c46e78b9f0014ac1
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-21 15:05:17 +0100

    add forms and small changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeroComponent } from './hero/hero.component';
 import { CarsComponent } from './cars/cars.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { CarsComponent } from './cars/cars.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+firstForm = new FormGroup({
+  email: new FormControl(''), 
+  message: new FormControl('') 
+})
+secondForm = new FormGroup({
+  fName: new FormControl(''), 
+  lName: new FormControl(''), 
+  age: new FormControl(''), 
+  email: new FormControl(''), 
+  phoneNumber: new FormControl(''), 
+  message: new FormControl('') 
+})
 
   constructor() { }
 
