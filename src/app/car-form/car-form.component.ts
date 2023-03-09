fdf9b7cd5a9dd1eb83376d070cef89ab861e56b9
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-03-09 09:59:49 +0100

    finish car-form

--- src/app/car-form/car-form.component.ts
+++ src/app/car-form/car-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, NgForm} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import Swal from 'sweetalert2';
 import { cars, Icars } from '../cars';
 
@@ -9,28 +9,38 @@ import { cars, Icars } from '../cars';
   styleUrls: ['./car-form.component.scss']
 })
 export class CarFormComponent implements OnInit {
-  cars: Icars[] = [];
-  car = {} as Icars;
+  newCars: Array<Icars> = cars;
   
   constructor() { }
 
 
   carForm = new FormGroup({
-    brand: new FormControl(''),
-    model: new FormControl(''),
-    insurance: new FormControl(''),
-    pricePerDay: new FormControl(''),
-    fuelType: new FormControl(''),
-    seatNum: new FormControl(''),
-    image: new FormControl(''),
+    brand: new FormControl('', Validators.required),
+    model: new FormControl('', Validators.required),
+    insurance: new FormControl('', Validators.required),
+    pricePerDay: new FormControl('', Validators.required),
+    fuelType: new FormControl('', Validators.required),
+    seatNum: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
 
   })
 
-  // returnCarForm(car: FormGroup){
-  //   this.cars.push(car.value);
+   addCar(){
+    let error = document.getElementById("error") as HTMLElement;
+    let formDiv = document.getElementById("formDiv") as HTMLElement;
 
-  //   console.log(this.cars)
-  // }
+    if (this.carForm.valid) {
+      let myVal: any = this.carForm.value;
+      this.newCars.push(myVal);
+    } else {
+      error.style.color = "red";
+      formDiv.style.border = "2px solid red";
+      error.innerHTML = "Please provide some information in every input field!";
+    }
+     
+
+    console.log(this.newCars);
+  }
 
   printForm(){
     console.table(this.carForm.value);
